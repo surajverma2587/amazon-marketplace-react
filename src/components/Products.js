41cea73009f1ea42f3e914a82255b6eed2e8600b
surajverma2587 <surajverma2587@gmail.com>
2023-01-25 20:39:45 +0000

    wip

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,3 +1,25 @@
-export const Products = () => {
-  return <div>Products</div>;
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import { ProductCard } from "./ProductCard";
+
+export const Products = ({ products }) => {
+  return (
+    <Box>
+      {products.length === 0 ? (
+        <Alert severity="error">No items found. Please try again later.</Alert>
+      ) : (
+        <Stack
+          direction="row"
+          sx={{
+            flexWrap: "wrap",
+          }}
+        >
+          {products.map((product) => (
+            <ProductCard {...product} key={product.ASIN} />
+          ))}
+        </Stack>
+      )}
+    </Box>
+  );
 };
